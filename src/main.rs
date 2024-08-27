51d184cf017d36292f0af70f3c99865f61baef8d
Craig P. Motlin <cmotlin@gmail.com>
2024-08-27 12:25:39 -0400

    Refactor so that output can be streaming.

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,11 @@ fn setup_logging(debug: bool) {
         .unwrap();
 }
 
-fn get_commit_message(sha: &str) -> String {
-    let clean_sha = sha.replace("^{tree}", "");
-    debug!("Fetching commit message for SHA: {}", clean_sha);
+fn get_commit_message(treeish: &str) -> String {
+    let commit_sha: String = treeish.replace("^{tree}", "");
+    debug!("Fetching commit message for SHA: {}", commit_sha);
 
-    let command = format!("git log --format=%B -n 1 {}", clean_sha);
+    let command = format!("git log --format=%B -n 1 {}", commit_sha);
     debug!("Running command: {}", command);
 
     match Command::new("sh")
@@ -39,37 +39,32 @@ fn get_commit_message(sha: &str) -> String {
                     .chars()
                     .take(120)
                     .collect::<String>();
-                debug!("Commit message retrieved: {}...", &message.chars().take(50).collect::<String>());
+                debug!("Commit message retrieved: {}...", &message.chars().take(120).collect::<String>());
                 message
             } else {
-                error!("Error fetching commit message for {}: {}", sha, String::from_utf8_lossy(&output.stderr));
+                error!("Error fetching commit message for {}: {}", treeish, String::from_utf8_lossy(&output.stderr));
                 "Commit message not available".to_string()
             }
         }
         Err(e) => {
-            error!("Failed to execute git command for {}: {}", sha, e);
+            error!("Failed to execute git command for {}: {}", treeish, e);
             "Commit message not available".to_string()
         }
     }
 }
 
 fn process_git_log<R: BufRead, W: Write>(reader: R, writer: &mut W) -> io::Result<()> {
-    let sha_regex = Regex::new(r"^[a-f0-9]{40}").unwrap();
-    let mut lines = Vec::new();
+    let sha_regex: Regex = Regex::new(r"^[a-f0-9]{40}").unwrap();
+
     let mut max_line_length = 0;
 
-    // Read all lines, storing them and calculating max length
     for line in reader.lines() {
-        let line = line?;
+        let line: String = line?;
         max_line_length = max_line_length.max(line.len());
-        lines.push(line);
-    }
 
-    // Process and write lines
-    for line in lines {
         if sha_regex.is_match(&line) {
-            let sha = line.split_whitespace().next().unwrap();
-            let commit_message = get_commit_message(sha);
+            let treeish: &str = line.split_whitespace().next().unwrap();
+            let commit_message: String = get_commit_message(treeish);
             writeln!(writer, "{:<width$} | {}", line, commit_message, width = max_line_length)?;
         } else {
             writeln!(writer, "{}", line)?;
